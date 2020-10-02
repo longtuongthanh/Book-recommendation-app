ae5f37559e86089e1623de5ad1abf2be36af8529
Long Tưởng Thành <18520227@ms.uit.edu.vn>
2020-10-02 12:07:10 +0700

    sign in screen. untested new form

--- BookRecommendationApp/BookRecommendationApp/Form1.cs
+++ BookRecommendationApp/BookRecommendationApp/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookRecommendationApp.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,26 @@ using System.Windows.Forms;
 
 namespace BookRecommendationApp
 {
-    public partial class Form1 : Form
+    public partial class FormSignIn : Form
     {
-        public Form1()
+        public FormSignIn()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var task = Database.Load(textBox1.Text, textBox2.Text);
+            task.Wait();
+            if (task.Result)
+            {
+                FormMain main = new FormMain();
+                main.Show(this);
+                main.FormClosing += (obj, args) => this.Visible = true;
+                this.Visible = false;
+            }
+        }
     }
+
+    public partial class FormMain : Form { }
 }
--- BookRecommendationApp/BookRecommendationApp/Model/Database.cs
+++ BookRecommendationApp/BookRecommendationApp/Model/Database.cs
@@ -14,23 +14,7 @@ namespace BookRecommendationApp.Model
 {
     public partial class Database
     {
-        private const string SignUpFailedPrompt = 
-            "Đã có lỗi xảy ra. Không đăng kí được.";
-        private const string SignUpYesNoPromptContent = 
-            "Không nhận ra bạn. Bạn có muốn đăng kí?";
-        private const string SignUpPromptTitle = "Đăng kí?";
-        private const string SignInFailedPrompt 
-            = "Không thể đăng nhập. Xin thử lại sau ít phút.";
-        #region FirebaseSetting
-        // TODO: export to file
-        static private string firebaseApiKey = "AIzaSyDu098TxwLgFJsfaenUPBfC1z9jyGGT2N8";
-        static private string databaseURL = "https://fir-test-bd7d1.firebaseio.com";
-
-        static private FirebaseAuthProvider authProvider;
-        static private FirebaseClient client = null;
-        static private FirebaseAuthLink token;
-        #endregion
-
+        #region Database
         private static List<Book> s_books;
         private static List<string> s_author;
         private static Setting s_setting;
@@ -41,7 +25,30 @@ namespace BookRecommendationApp.Model
         static public List<string> Tags => s_tags;
         static public User User => s_user;
         static public Setting Setting => s_setting;
-        
+        #endregion
+
+        #region ConstantString
+        private const string SignUpFailedPrompt =
+            "Đã có lỗi xảy ra. Không đăng kí được.";
+        private const string SignUpYesNoPromptContent =
+            "Không nhận ra bạn. Bạn có muốn đăng kí?";
+        private const string SignUpPromptTitle = "Đăng kí?";
+        private const string SignInFailedPrompt
+            = "Không thể đăng nhập. Xin thử lại sau ít phút.";
+        #endregion
+
+        #region FirebaseSetting
+        // TODO: export to file
+        static private string firebaseApiKey = "AIzaSyDu098TxwLgFJsfaenUPBfC1z9jyGGT2N8";
+        static private string databaseURL = "https://fir-test-bd7d1.firebaseio.com";
+
+        static private FirebaseAuthProvider authProvider =
+            new FirebaseAuthProvider(new FirebaseConfig(firebaseApiKey));
+        static private FirebaseClient client = null;
+        static private FirebaseAuthLink token = null;
+        #endregion
+
+        #region LoadData
         static private void SignUp(string email, string password)
         {
             var authActionSignUp = authProvider.CreateUserWithEmailAndPasswordAsync(email, password);
@@ -81,22 +88,64 @@ namespace BookRecommendationApp.Model
                 });
         }
 
-        public static bool Load(string username, string password)
+        #region Tasks
+        private static async Task<bool> LoadBook()
+        {
+            var task = client.Child("Books").OnceAsync<Book>();
+            var taskResult = await task;
+            s_books = taskResult.Select(item => item.Object).ToList();
+            return task.IsFaulted;
+        }
+        private static async Task<bool> LoadTags()
+        {
+            var task = client.Child("Tags").OnceAsync<string>();
+            var taskResult = await task;
+            s_tags = taskResult.Select(item => item.Object).ToList();
+            return task.IsFaulted;
+        }
+        private static async Task<bool> LoadUser()
+        {
+            var task = client.Child("Users").Child(token.FirebaseToken).OnceSingleAsync<User>();
+            var taskResult = await task;
+            s_user = taskResult;
+            return task.IsFaulted;
+        }
+        private static async Task<bool> LoadSetting()
+        {
+            var task = client.Child("Setting").OnceSingleAsync<Setting>();
+            var taskResult = await task;
+            s_setting = taskResult;
+            return task.IsFaulted;
+        }
+        #endregion
+        public static async Task<bool> Load(string username, string password)
         {
             if (client == null)
                 client = SignIn(username, password);
             if (client == null)
                 return false;
-            var task1 = client.Child("Books").OnceAsync<Book>();
-            var task2 = client.Child("Tags").OnceAsync<string>();
-            var task3 = client.Child("Users").Child(token.FirebaseToken).OnceAsync<Book>();
-            var task4 = client.Child("Setting").OnceSingleAsync<Setting>();
-            
+
+            var task1 = LoadBook();
+            var task2 = LoadTags();
+            var task3 = LoadUser();
+            var task4 = LoadSetting();
+
+            await task1;
+            await task2;
+            await task3;
+            await task4;
+
             return true;
         }
+        #endregion
+
+        #region CloseFirebase
         public static void Close()
         {
-            
+            client.Dispose();
+            client = null;
+            token = null;
         }
+        #endregion
     }
 }
