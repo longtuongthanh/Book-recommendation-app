75e1da582cec398388073dce31b3cf030ad9d0ee
Long Tưởng Thành <18520227@ms.uit.edu.vn>
2020-11-30 13:18:54 +0700

    add book form resize

--- BookRecommendationApp/BookRecommendationApp/AddBooks.Designer.cs
+++ BookRecommendationApp/BookRecommendationApp/AddBooks.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.picture = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -38,16 +39,16 @@
             this.labelAuthor = new System.Windows.Forms.Label();
             this.labelName = new System.Windows.Forms.Label();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(493, 497);
+            this.button1.Location = new System.Drawing.Point(370, 404);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(115, 31);
+            this.button1.Size = new System.Drawing.Size(86, 25);
             this.button1.TabIndex = 1;
             this.button1.Text = "thêm";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,10 +57,10 @@
             // picture
             // 
             this.picture.Image = global::BookRecommendationApp.Properties.Resources._130304;
-            this.picture.Location = new System.Drawing.Point(23, 4);
-            this.picture.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.picture.Location = new System.Drawing.Point(17, 3);
+            this.picture.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.picture.Name = "picture";
-            this.picture.Size = new System.Drawing.Size(135, 199);
+            this.picture.Size = new System.Drawing.Size(101, 162);
             this.picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picture.TabIndex = 5;
             this.picture.TabStop = false;
@@ -67,6 +68,7 @@
             // 
             // panel2
             // 
+            this.panel2.AutoSize = true;
             this.panel2.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.panel2.Controls.Add(this.label1);
             this.panel2.Controls.Add(this.textBox3);
@@ -75,86 +77,105 @@
             this.panel2.Controls.Add(this.labelDesc);
             this.panel2.Controls.Add(this.labelAuthor);
             this.panel2.Controls.Add(this.labelName);
-            this.panel2.Location = new System.Drawing.Point(12, 48);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Location = new System.Drawing.Point(9, 39);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(596, 418);
+            this.panel2.Size = new System.Drawing.Size(447, 340);
             this.panel2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 100);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Link";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(238, 123);
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(178, 100);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(325, 22);
+            this.textBox3.Size = new System.Drawing.Size(245, 20);
             this.textBox3.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(238, 64);
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(178, 52);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(325, 22);
+            this.textBox2.Size = new System.Drawing.Size(245, 20);
             this.textBox2.TabIndex = 3;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(238, 10);
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(178, 8);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(325, 22);
+            this.textBox1.Size = new System.Drawing.Size(245, 20);
             this.textBox1.TabIndex = 2;
             // 
             // labelDesc
             // 
             this.labelDesc.AutoSize = true;
-            this.labelDesc.Location = new System.Drawing.Point(152, 195);
+            this.labelDesc.Location = new System.Drawing.Point(114, 158);
+            this.labelDesc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelDesc.Name = "labelDesc";
-            this.labelDesc.Size = new System.Drawing.Size(65, 17);
+            this.labelDesc.Size = new System.Drawing.Size(50, 13);
             this.labelDesc.TabIndex = 1;
             this.labelDesc.Text = "Nội dung";
             // 
             // labelAuthor
             // 
             this.labelAuthor.AutoSize = true;
-            this.labelAuthor.Location = new System.Drawing.Point(152, 64);
+            this.labelAuthor.Location = new System.Drawing.Point(114, 52);
+            this.labelAuthor.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelAuthor.Name = "labelAuthor";
-            this.labelAuthor.Size = new System.Drawing.Size(55, 17);
+            this.labelAuthor.Size = new System.Drawing.Size(43, 13);
             this.labelAuthor.TabIndex = 1;
             this.labelAuthor.Text = "Tác giả";
             // 
             // labelName
             // 
             this.labelName.AutoSize = true;
-            this.labelName.Location = new System.Drawing.Point(152, 10);
+            this.labelName.Location = new System.Drawing.Point(114, 8);
+            this.labelName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelName.Name = "labelName";
-            this.labelName.Size = new System.Drawing.Size(33, 17);
+            this.labelName.Size = new System.Drawing.Size(26, 13);
             this.labelName.TabIndex = 1;
             this.labelName.Text = "Tên";
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(250, 221);
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(188, 180);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(325, 213);
+            this.richTextBox1.Size = new System.Drawing.Size(245, 174);
             this.richTextBox1.TabIndex = 7;
             this.richTextBox1.Text = "";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(152, 123);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 17);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Link";
-            // 
             // AddBooks
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(982, 583);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(736, 474);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.picture);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "AddBooks";
             this.Text = "AddBooks";
             this.Load += new System.EventHandler(this.AddBooks_Load);
@@ -162,6 +183,7 @@
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
